ef8d8bbcca0b5bf1f6d55b0ef67292ca24b5c95b
rahmeesha reynolds <crazym.weird@gmail.com>
2023-01-09 19:49:43 -0500

    "finally fixed it!!"

--- clock.js
+++ clock.js
@@ -1,21 +1,45 @@
-const secondHand = document.querySelector('.second');
-const minuteHand = document.querySelector('.minute');
-const hourHand = document.querySelector('.hour');
+const secondHand = document.querySelector('[data-second-hand]');
+const minuteHand = document.querySelector('[data-minute-hand]');
+const hourHand = document.querySelector('[data-hour-hand]');
+
+setInterval(setDate, 1000)
+
+// function setClock() {
+//     const currentDate = new Date()
+//     const secondsRatio = currentDate.getSeconds() / 60
+//     const minutesRatio = (secondsRatio + currentDate.getMinutes()) / 60
+//     const hoursRatio =(minutesRatio + currentDate.getHours()) /12
+//     setRotation(secondHand, secondsRatio)
+//     setRotation(minuteHand, minutesRatio)
+//     setRotation(hourHand, hoursRatio) 
+// }
+
+// function setRotation(element, rotationRatio) {
+//     element.style.setProperty('--rotation', rotationRatio * 360)
+// }
+
+// setClock()
+
 
 function setDate() {
     const now = new Date();
 
     const second = now.getSeconds();
-    const secondDegrees = ((second / 60) * 360) + 90;
-    secondHand.style.transform = 'rotate(${secondDegrees}deg)';
+    const secondDegrees = second * 6;
+    secondHand.style.transform = `rotate(${secondDegrees}deg)`;
 
     const minute = now.getMinutes();
-    const minuteDegrees = ((mins / 60) * 360) + 90 ;
-    minuteHand.style.transform = 'rotate(${minuteDegrees}deg)';
+    const minuteDegrees = minute * 6;
+    minuteHand.style.transform = `rotate(${minuteDegrees}deg)`;
 
-    const hour = now.getMinutes();
-    const hourDegrees = ((mins / 12) * 360) + 90;
-    hourHand.style.transform = 'rotate(${hourDegrees}deg)';
+    let hour = now.getHours();
+    
+    //if hour is more than 12 subtract 12 from it
+    if (hour > 12)  {       
+         hour =hour - 12; 
+}
+    const hourDegrees = hour * 30;
+    hourHand.style.transform = `rotate(${hourDegrees}deg)`;
 }
 
-setInterval(setDate, 1000);
+
